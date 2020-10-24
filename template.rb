e360c89af0d70676fce1aa76dcac59b6ee9bf905
irisbune <irisbune@gmail.com>
2020-10-24 16:37:22 +0200

    Create omniauth controller

--- template.rb
+++ template.rb
@@ -93,6 +93,25 @@ def setup_google_omniauth
         :image_size => 250
       }
 
-    HEREDOC
+    CODE
+  end
+
+  create_file "app/controllers/users/omniauth_callbacks_controller.rb" do
+    <<-CODE
+    class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+      def google_oauth2
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+
+        if @user&.persisted?
+          flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+          sign_in_and_redirect @user, :event => :authentication
+        else
+          flash[:error] = "Sorry, you can't sign up with that email address"
+          session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
+          redirect_to root_path
+        end
+      end
+    end
+    CODE
   end
 end
