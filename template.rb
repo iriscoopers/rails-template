42e837752e439df0cedff35e15e575774aa8b324
irisbune <irisbune@gmail.com>
2020-10-25 11:13:54 +0100

    Improve devise and omniauth setup

--- template.rb
+++ template.rb
@@ -40,57 +40,69 @@ def setup_devise
     gsub_file migration, /:admin/, ":admin, default: false"
   end
 
-  insert_into_file "app/controllers/application_controller.rb", before: "end" do
-    "before_action :authenticate_user!"
+  insert_into_file "config/routes.rb", before: "devise_for :users" do <<-CODE
+  devise_scope :user do
+    root to: 'devise/sessions#new'
   end
 
-  rails_command "be db:migrate"
-  generate "devise:views -v sessions"
+  CODE
+  end
+
+  insert_into_file "app/controllers/application_controller.rb", before: "end" do
+    "  before_action :authenticate_user!\n"
+  end
+
+  rails_command "db:migrate"
   create_file "app/views/devise/sessions/new.html.slim" do
-    <<-CODE
-    h2= "Log in"
-    = render "devise/shared/links"
+    <<~CODE
+      h2= "Log in"
+      = render "devise/shared/links"
+    CODE
+  end
+
+  create_file "app/views/devise/shared/_links.html.slim" do
+    <<~CODE
+      = link_to "Sign in with Gmail", user_google_oauth2_omniauth_authorize_path, class: "btn"
     CODE
   end
 end
 
 def setup_google_omniauth
-  inject_into_file "app/models/user.rb", before: "end" do
-    <<-CODE
-               :omniauthable, :omniauth_providers => [:google_oauth2]
+  insert_into_file "app/models/user.rb", before: "end" do <<-CODE
+         :omniauthable, :omniauth_providers => [:google_oauth2]
 
-    def self.from_omniauth(auth)
-      data = auth.info
+  def self.from_omniauth(auth)
+    data = auth.info
 
-      user = User.where(email: data["email"]).first
+    user = User.where(email: data["email"]).first
 
-      if user
-        user.update_attribute(:avatar, data["image"])
-      else
-        return false if User.count == 1
+    if user
+      user.update_attribute(:avatar, data["image"])
+    else
+      return false if User.count == 1
 
-        user = User.create(
-          email: data["email"],
-          password: Devise.friendly_token[0,20],
-          avatar: data["image"]
-        )
-      end
-
-      user
+      user = User.create(
+        email: data["email"],
+        password: Devise.friendly_token[0,20],
+        avatar: data["image"]
+      )
     end
 
-    def self.new_with_session(params, session)
-      super.tap do |user|
-        if data = session["devise.google_oauth2_data"] && session["devise.google_oauth2_data"]["extra"]["raw_info"]
-          user.email = data["email"] if user.email.blank?
-        end
+    user
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.google_oauth2_data"] && session["devise.google_oauth2_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
       end
     end
-    CODE
+  end
+CODE
   end
 
   inject_into_file "config/initializers/devise.rb", before: "# ==> Warden configuration" do
-    <<-CODE
+    <<~CODE
       config.omniauth :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {
         access_type: "offline",
         approval_prompt: "",
@@ -103,21 +115,21 @@ def setup_google_omniauth
   end
 
   create_file "app/controllers/users/omniauth_callbacks_controller.rb" do
-    <<-CODE
-    class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-      def google_oauth2
-        @user = User.from_omniauth(request.env["omniauth.auth"])
+    <<~CODE
+      class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+        def google_oauth2
+          @user = User.from_omniauth(request.env["omniauth.auth"])
 
-        if @user&.persisted?
-          flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-          sign_in_and_redirect @user, :event => :authentication
-        else
-          flash[:error] = "Sorry, you can't sign up with that email address"
-          session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
-          redirect_to root_path
+          if @user&.persisted?
+            flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+            sign_in_and_redirect @user, :event => :authentication
+          else
+            flash[:error] = "Sorry, you can't sign up with that email address"
+            session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
+            redirect_to root_path
+          end
         end
       end
-    end
     CODE
   end
 end
