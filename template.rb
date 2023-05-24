c0becb30115501059e9da5e14bc091f9840a32ef
iriscoopers <bighairamazon@gmail.com>
2023-05-24 09:27:13 +0200

    Improve template with more steps

--- template.rb
+++ template.rb
@@ -1,48 +1,20 @@
 # frozen_string_literal: true
 
-gem "devise"
-gem "slim-rails"
-gem "omniauth-google-oauth2"
-gem "httparty"
+environment
+  <<~CODE
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, fixture: false
+      g.stylesheets     false
+    end
+  CODE
 
-gem_group :development, :test do
-  gem "rspec-rails"
-  gem "dotenv-rails"
-end
+@project_name = ask("What is this project's name?")
 
-run "bundle install"
-
-create_file ".env" do
-  key = ask("What is your Google api's key?")
-  secret = ask("What is your Google api's secret?")
-  "GOOGLE_KEY=#{key}\nGOOGLE_SECRET=#{secret}"
-end
-
-insert_into_file ".gitignore", after: "/config/master.key" do
-  "\n.env"
-end
-
-environment "config.generators { |g| g.template_engine :slim }"
-
-def setup_devise
-  run "spring stop && spring start"
+def add_devise
   generate "devise:install"
   environment "config.action_mailer.default_url_options = { host: \"localhost\", port: 3000 }",
     env: "development"
-  generate :devise, "User", "avatar:string", "admin:boolean"
-
-  # set default value for admin to false
-  in_root do
-    migration = Dir.glob("db/migrate/*").max_by { |f| File.mtime(f) }
-    gsub_file migration, /:admin/, ":admin, default: false"
-  end
-
-  insert_into_file "app/controllers/application_controller.rb", before: "end" do
-    "  protect_from_forgery with: :exception\n"
-    "  before_action :authenticate_user!\n"
-  end
-
-  rails_command "db:migrate"
 
   create_file "app/controllers/registrations_controller.rb" do
     <<~CODE
@@ -62,21 +34,38 @@ def setup_devise
     CODE
   end
 
-  create_file "app/views/devise/sessions/new.html.slim" do
+  create_file "app/views/devise/sessions/new.html.haml" do
     <<~CODE
-      h2= "Log in"
+      %h2 Log in
       = render "devise/shared/links"
     CODE
   end
 
-  create_file "app/views/devise/shared/_links.html.slim" do
+  create_file "app/views/devise/shared/_links.html.haml" do
     <<~CODE
       = link_to "Sign in with Gmail", user_google_oauth2_omniauth_authorize_path, class: "btn"
     CODE
   end
 end
 
-def setup_google_omniauth
+def scaffold_user
+  generate :devise, "User", "avatar:string", "admin:boolean"
+
+  # set default value for admin to false
+  in_root do
+    migration = Dir.glob("db/migrate/*").max_by { |f| File.mtime(f) }
+    gsub_file migration, /:admin/, ":admin, default: false"
+  end
+
+  insert_into_file "app/controllers/application_controller.rb", before: "end" do
+    "  protect_from_forgery with: :exception\n"
+    "  before_action :authenticate_user!\n"
+  end
+
+  rails_command "db:migrate"
+end
+
+def add_google_omniauth
   in_root do
     gsub_file "app/models/user.rb", /:validatable/, ":validatable,"
   end
@@ -86,23 +75,20 @@ def setup_google_omniauth
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
   def self.from_omniauth(auth)
+    return false if User.count == 1
+
     data = auth.info
 
-    user = User.where(email: data["email"]).first
-
-    if user
+    if user = User.where(email: data["email"]).first
       user.update_attribute(:avatar, data["image"])
-    else
-      return false if User.count == 1
-
-      user = User.create(
-        email: data["email"],
-        password: Devise.friendly_token[0,20],
-        avatar: data["image"]
-      )
+      return
     end
 
-    user
+    User.create(
+      email: data["email"],
+      password: Devise.friendly_token[0,20],
+      avatar: data["image"]
+    )
   end
 
   def self.new_with_session(params, session)
@@ -117,13 +103,16 @@ CODE
 
   insert_into_file "config/initializers/devise.rb", before: "# ==> Warden configuration" do
     <<~CODE
-      config.omniauth :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], {
-        access_type: "offline",
-        approval_prompt: "",
-        prompt: "select_account",
-        :image_aspect_ratio => "square",
-        :image_size => 250
-      }
+      config.omniauth :google_oauth2,
+        Rails.application.credentials.google_key,
+        Rails.application.credentials.google_secret,
+        {
+          access_type: "offline",
+          approval_prompt: "",
+          prompt: "select_account",
+          :image_aspect_ratio => "square",
+          :image_size => 250
+        }
 
     CODE
   end
@@ -146,7 +135,9 @@ CODE
       end
     CODE
   end
+end
 
+def add_devise_routes
   in_root do
     gsub_file "config/routes.rb", /  devise_for(.*?)routing\.html\n/m,
 <<-CODE
@@ -165,137 +156,78 @@ CODE
   end
 end
 
-def setup_materialize
-  run "yarn add materialize-css jquery"
-
-  insert_into_file "config/webpack/environment.js", after: "require('@rails/webpacker')\n" do
-    <<~CODE
-      const webpack = require('webpack')
-      environment.plugins.append('Provide', new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-      }))
-
-    CODE
-  end
-
-  insert_into_file "app/javascript/packs/application.js", before: "require(\"@rails/ujs\")" do
-    <<~CODE
-      import "../stylesheets/application.sass";
-      import "materialize-css/dist/js/materialize"
-      import "jquery/dist/jquery"
-
-    CODE
-  end
-
-  insert_into_file "app/javascript/packs/application.js", after: "require(\"channels\")\n" do
-    <<~CODE
-      $(document).on("turbolinks:load", function(e) {
-        M.updateTextFields();
-        $(".button-collapse").sidenav();
-      });
-    CODE
-  end
-
-  create_file "app/javascript/stylesheets/application.sass" do
-    <<~CODE
-      @import "materialize-css/dist/css/materialize"
-      @import "variables"
-      @import "base"
-    CODE
-  end
-
-  create_file "app/javascript/stylesheets/variables.sass" do
-    <<~CODE
-      $primary-color: #2F4858 !default
-      $secondary-color: #FFBB00 !default
-    CODE
-  end
-
-  create_file "app/javascript/stylesheets/base.sass" do
-<<-CODE
-nav
-  background-color: $primary-color
-CODE
-  end
+def scaffold_pages
+  generate "scaffold", "Pages title:string description:text, user:references"
+  rails "db:migrate"
 end
 
-def setup_pages
-  create_file "app/controllers/pages_controller.rb" do
+def import_tailwind
+  insert_into_file "app/assets/stylesheets/application.tailwind.css", before: "@tailwind base;" do
     <<~CODE
-      class PagesController < ApplicationController
-        def index; end
-      end
-    CODE
-  end
+      @import url('https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;500;600&display=swap');
 
-  create_file "app/views/pages/index.html.slim" do
-    <<~CODE
-        h2="Home"
     CODE
   end
 end
 
-def setup_views
+def add_layout
   remove_file "app/views/layouts/application.html.erb"
 
-  create_file "app/views/layouts/application.html.slim" do
+  create_file "app/views/layouts/application.html.haml" do
     <<~CODE
-      doctype html
-      html
-        head
-          title= "Template"
-          meta name="keywords" content="Template"
-          meta name="author" content="author"
-          meta charset="utf-8"
-          meta name="viewport" content="width=device-width,initial-scale=1"
-
-          = stylesheet_link_tag "https://fonts.googleapis.com/icon?family=Material+Icons"
-          = stylesheet_link_tag "application", media: "all", "data-turbolinks-track": "reload"
-          = javascript_pack_tag "application", "data-turbolinks-track": "reload"
+      !!!
+      %html
+        %head
+          %title #{@project_name}
+          %meta{name: "viewport", content: "width=device-width,initial-scale=1"}
           = csrf_meta_tags
+          = csp_meta_tag
+          = stylesheet_link_tag "tailwind", "inter-font", "data-turbo-track": "reload"
+          = stylesheet_link_tag "application", "data-turbo-track": "reload"
+          = javascript_importmap_tags
 
-        body
+        %body
           = render "shared/navigation"
           = render "shared/messages"
 
-          .container class="\#{controller_name} \#{action_name}"
+          %main.container.mx-auto.mt-28.px-5.flex
             = yield
     CODE
   end
 
-  create_file "app/views/shared/_messages.html.slim" do
+  create_file "app/views/shared/_messages.html.haml" do
   end
 
-  create_file "app/views/shared/_navigation.html.slim" do
+  create_file "app/views/shared/_navigation.html.haml" do
     <<~CODE
       nav
-        .nav-wrapper
-          = link_to "Template", root_path, class: "brand-logo"
-          = link_to content_tag(:i, "menu", class: "material-icons"), "#", data: { target: "mobile" }, class: "sidenav-trigger"
+        .mx-auto max-w-7xl px-2 sm:px-6 lg:px-8
+          = link_to #{@project_name}, root_path, class: "brand-logo"
+          = link_to content_tag(:i, "menu", class: "text-white p-2 ml-2 cursor-pointer"), "#", data: { target: "mobile" }, class: "sidenav-trigger"
 
-          ul.right.hide-on-med-and-down
+          %ulul.right
             - if current_user
-              li= link_to "Home", root_path
-              li= link_to "Logout", destroy_user_session_path, method: :delete
+              %li= link_to "Home", root_path
+              %li= link_to "Logout", destroy_user_session_path, method: :delete
             - else
-              li= link_to "Login", new_user_session_path
+              %li= link_to "Login", new_user_session_path
 
-          ul.sidenav#mobile
+          %ul.sidenav#mobile
             - if current_user
-              li= link_to "Home", root_path
-              li= link_to "Logout", destroy_user_session_path, method: :delete
+              %li= link_to "Home", root_path
+              %li= link_to "Logout", destroy_user_session_path, method: :delete
             - else
-              li= link_to "Login", new_user_session_path
+              %li= link_to "Login", new_user_session_path
     CODE
   end
 end
 
 after_bundle do
-  setup_devise
-  setup_google_omniauth
-  setup_materialize
-  setup_pages
-  setup_views
+  add_devise
+  scaffold_user
+  add_google_omniauth
+  add_devise_routes
+  scaffold_pages
+  import_tailwind
+  add_views
 end
